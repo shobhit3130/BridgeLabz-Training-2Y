979b98fdb2946224b3906651bc91fcd917b4d9a4
shobhit3130 <shobhitg3130@gmail.com>
2025-11-10 14:06:39 +0530

    new assignment

--- oops_constructor/ATM.java
+++ oops_constructor/ATM.java
@@ -1,38 +1,4 @@
-class BankAccount {
-    String accountHolder;
-    int accountNumber;
-    double balance;
-
-    BankAccount(String accountHolder, int accountNumber, double balance) {
-        this.accountHolder = accountHolder;
-        this.accountNumber = accountNumber;
-        this.balance = balance;
-    }
-
-    void deposit(double amount) {
-        balance += amount;
-        System.out.println("Deposited: " + amount);
-    }
-
-    void withdraw(double amount) {
-        if (amount <= balance) {
-            balance -= amount;
-            System.out.println("Withdrew: " + amount);
-        } else {
-            System.out.println("Insufficient balance!");
-        }
-    }
-
-    void displayBalance() {
-        System.out.println("Current Balance: " + balance);
-    }
-}
+package PACKAGE_NAME;
 
 public class ATM {
-    public static void main(String[] args) {
-        BankAccount acc = new BankAccount("Saksham", 333333, 100000);
-        acc.deposit(500);
-        acc.withdraw(300);
-        acc.displayBalance();
-    }
 }
